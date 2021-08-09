370c7230e3a618ae13213109f6cf81658e08b49d
afluffysquirrel <afluffysquirrel@gmail.com>
2021-08-09 12:45:46 +0100

    PROD deployment

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
 from website import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0', port=80)
