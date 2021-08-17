05f379934095f4d48d72e55f5ef28bf8e8c507f0
afluffysquirrel <afluffysquirrel@gmail.com>
2021-08-17 11:42:56 +0100

    Search element visibility

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -2,6 +2,12 @@ $(function() {
     $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
     navbar_height = document.querySelector('.navbar').offsetHeight;
     document.body.style.paddingTop = navbar_height + 'px';
+    if(location.pathname.split("/")[1] == "articles" || location.pathname.split("/")[1] == "tickets"){
+        document.getElementById('search').style.visibility='visible';
+    } else 
+    {
+        document.getElementById('search').style.visibility='hidden';
+    }
 });
 
 /*
