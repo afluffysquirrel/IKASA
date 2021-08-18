186262c89cf27f375fd9e05eeced70545e6799e3
afluffysquirrel <afluffysquirrel@gmail.com>
2021-08-18 11:31:22 +0100

    JS article validation

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -2,17 +2,19 @@ $(function() {
     $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
     navbar_height = document.querySelector('.navbar').offsetHeight;
     document.body.style.paddingTop = navbar_height + 'px';
+
     if(location.pathname.split("/")[1] == "articles" || location.pathname.split("/")[1] == "tickets"){
         document.getElementById('search').style.visibility='visible';
     } else 
     {
         document.getElementById('search').style.visibility='hidden';
     }
+    setInterval(function() {
+        if(document.getElementById('title').value.length == 0 || document.getElementById('tags').value.length == 0){
+            document.getElementById('submit-btn').disabled = true;
+        }
+        else {
+            document.getElementById('submit-btn').disabled = false;
+        }
+    }, 1000); // Wait 1000ms before running again
 });
-
-/*
-function search_redirect() {
-    window.location.href += '?search='+document.getElementById("search").value;
-}
-document.getElementById("search_button").addEventListener("click", search_redirect);
-*/
